005a0cf1c4020a6572a65910886271d1ca006eba
Sanaz <snz5seifi@gmail.com>
2022-03-02 15:10:28 -0500

    app component bug fixed

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import './App.css';
 
 function App(){
   const [value, setValue] = useState("");
+  return(
   <div className='Sanaz'>
     <input value={value} onClick = {(e) => setValue(e.target.value)} />
     <p>{value}</p>
 
   </div>
+    
+  )
 }
 
 export default App;
