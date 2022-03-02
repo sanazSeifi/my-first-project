cc4d3e72f07a69086fde28f1ed42c28430aae5ae
Sanaz <snz5seifi@gmail.com>
2022-03-02 14:38:10 -0500

    initial message

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import { useState } from 'react';
+import './App.css';
+
+function App(){
+  const [value, setValue] = useState("");
+  <div className='Sanaz'>
+    <input value={value} onClick = {(e) => setValue(e.target.value)} />
+    <p>{value}</p>
+
+  </div>
+}
+
+export default App;
